345579f52f39beb0f8fa2c204091c172fed67d09
Lagadoo <fvcklagadoo@gmail.com>
2025-11-14 15:19:04 -0300

    feat(modular): modularizacao do codigo

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,24 @@
-print("Hello, world!")
+def ler_notas():
+    notas = []
+    for i in range(1, 4):
+        nota = float(input(f"Digite a {i}ª nota: "))
+        notas.append(nota)
+    return notas
+
+def calcular_media(notas):
+    return sum(notas) / len(notas)
+
+
+def verificar_situacao(media):
+    if media > 7:
+        return "Aprovado"
+    elif media >= 5:
+        return "Recuperação"
+    else:
+        return "Reprovado"
+
+
+def mostrar_resultado(nome, media, situacao):
+    print(f"Aluno: {nome}")
+    print(f"Media: {media:.2f}")
+    print(f"Situação: {situacao}")
--- sistema-notas.py
+++ sistema-notas.py
@@ -1,29 +1,6 @@
+from funcoes import ler_notas, calcular_media, verificar_situacao, mostrar_resultado
+
 print("Hello, world!")
-
-def ler_notas():
-    notas = []
-    for i in range(1, 4):
-        nota = float(input(f"Digite a {i}ª nota: "))
-        notas.append(nota)
-    return notas
-
-def calcular_media(notas):
-    return sum(notas) / len(notas)
-
-
-def verificar_situacao(media):
-    if media > 7:
-        return "Aprovado"
-    elif media >= 5:
-        return "Recuperação"
-    else:
-        return "Reprovado"
-
-
-def mostrar_resultado(nome, media, situacao):
-    print(f"Aluno: {nome}")
-    print(f"Media: {media:.2f}")
-    print(f"Situação: {situacao}")
 
 while True:
     nome = input("Digite o nome do aluno: ")
